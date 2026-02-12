38789dd147acb6949d33029013508eea39cad1a7
Chris Lane <chlane@paypal.com>
2026-02-12 10:41:27 -0800

    adding elpa files

---
+++ .emacs.d/elpa/sudoku-20191015.1315/sudoku-pkg.el
@@ -0,0 +1,10 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "sudoku" "20191015.1315"
+  "Simple sudoku game, can download puzzles."
+  '((emacs "24.4"))
+  :url "https://github.com/zevlg/sudoku.el"
+  :commit "b1924fd244a5fa284de9d67b66fbd69164b37318"
+  :revdesc "b1924fd244a5"
+  :keywords '("games")
+  :authors '(("Zajcev Evgeny" . "zevlg@yandex.ru"))
+  :maintainers '(("Zajcev Evgeny" . "zevlg@yandex.ru")))
